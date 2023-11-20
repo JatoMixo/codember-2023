5f00d44e4501362cef8551f3b5df445c769a8903
JatoMixo <jatomixo@toshiba.localhost>
2023-11-20 17:12:41 +0100

    Minor change

---
+++ challenge-01/target/debug/deps/challenge_01-148cb94b6575c0e3.d
@@ -0,0 +1,5 @@
+/home/jatomixo/Code/codember-2023/challenge-01/target/debug/deps/challenge_01-148cb94b6575c0e3.rmeta: src/main.rs
+
+/home/jatomixo/Code/codember-2023/challenge-01/target/debug/deps/challenge_01-148cb94b6575c0e3.d: src/main.rs
+
+src/main.rs:
---
+++ challenge-02/target/debug/deps/challenge_02-b9d6fe3a5f8bafc0.d
@@ -0,0 +1,5 @@
+/home/jatomixo/Code/codember-2023/challenge-02/target/debug/deps/challenge_02-b9d6fe3a5f8bafc0.rmeta: src/main.rs
+
+/home/jatomixo/Code/codember-2023/challenge-02/target/debug/deps/challenge_02-b9d6fe3a5f8bafc0.d: src/main.rs
+
+src/main.rs:
